8443e778721ce25ce11636d51967fe47a5b30ea3
Marcelo Linux <mppozzobon8@gmail.com>
2024-11-26 21:36:42 -0300

    Commit 15

--- src/Price.java
+++ src/Price.java
@@ -6,7 +6,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                getDaysRented() > 1) frequentRenterPoints++;
+        return 1;
     }
 }
