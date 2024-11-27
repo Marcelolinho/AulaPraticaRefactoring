6a944ae450b303d670812554f6e576c0a24892f8
Marcelo Linux <mppozzobon8@gmail.com>
2024-11-26 21:33:10 -0300

    Commit 13

--- src/Price.java
+++ src/Price.java
@@ -3,23 +3,5 @@ package src;
 public abstract class Price {
     public abstract int getPriceCode();
 
-    public double getCharge(int daysRented) {
-        private double thisAmount = 0;
-        switch (movie) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
+    public abstract double getCharge(int daysRented);
 }
