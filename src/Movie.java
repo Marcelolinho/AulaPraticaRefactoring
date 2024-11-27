46799da418342108877451daa5877af879cbbf3c
Marcelo Linux <mppozzobon8@gmail.com>
2024-11-26 21:28:01 -0300

    Commit 12

--- src/Movie.java
+++ src/Movie.java
@@ -38,23 +38,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        private double thisAmount = 0;
-        switch (movie) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
